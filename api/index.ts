6d6234acb82c1e8d729291dff277be0ff494151e
Kaung Htet Zaw <zawk37010@gmail.com>
2026-08-26 16:16:09 +0630

    modify

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,10 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { createApp } from '../src/app';
 
-export default createApp();
+const app = createApp();
+
+function handler(req: VercelRequest, res: VercelResponse): void {
+  app(req, res);
+}
+
+export = handler;
